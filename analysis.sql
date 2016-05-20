28a3be58fad7b3041a9017b9af29e01b7025e470
Dan Goldin <dangoldin@gmail.com>
2016-05-19 23:21:24 -0400

    More analysis

---
+++ analysis.sql
@@ -0,0 +1,19 @@
+select n.gender, t.production_year, avg(t.production_year - pb.birth_year) as avg_age, count(1) as cnt, count(distinct(t.id)) as num_movies
+from title t
+join cast_info ci on t.id = ci.movie_id and t.kind_id = 1
+join name n on n.id = ci.person_id
+join person_birth pb on n.id = pb.person_id
+group by n.gender, t.production_year;
+
+select n.gender, t.production_year, avg(t.production_year - pb.birth_year) as avg_age, count(1) as cnt, count(distinct(t.id)) as num_movies
+from title t
+join cast_info ci on t.id = ci.movie_id and t.kind_id = 1
+join name n on n.id = ci.person_id
+join person_birth pb on n.id = pb.person_id
+where ci.person_id in (
+  select person_id
+  from cast_info
+  group by person_id
+  having count(1) > 20
+)
+group by n.gender, t.production_year;
